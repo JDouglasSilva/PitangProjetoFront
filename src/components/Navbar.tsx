be71cca2b6859bbf9c104f7a5bdb76c5c7a76563
Dag <jddouglassilva@gmail.com>
2024-07-14 23:52:11 -0300

    Atualização da NavBar, para exibição de botões de redirecionamento

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Box, Flex, Heading, useColorModeValue } from '@chakra-ui/react';
+import { Box, Flex, Heading, useColorModeValue, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const bgColor = useColorModeValue('green.800', 'green.900');
   const textColor = useColorModeValue('white', 'white');
+  const buttonBgColor = useColorModeValue('green.600', 'green.700');
+  const buttonHoverBgColor = useColorModeValue('green.500', 'green.600');
 
   return (
     <Box bg={bgColor} px={4}>
@@ -12,6 +14,27 @@ const Navbar = () => {
         <Heading as={Link} to="/" color={textColor} size="md">
           Portal Vacinação COVID-19
         </Heading>
+        <Flex alignItems="center">
+          <Button 
+            as={Link} 
+            to="/formulario" 
+            bg={buttonBgColor} 
+            color={textColor} 
+            _hover={{ bg: buttonHoverBgColor }} 
+            mr={4}
+          >
+            Faça seu agendamento
+          </Button>
+          <Button 
+            as={Link} 
+            to="/agendamentos" 
+            bg={buttonBgColor} 
+            color={textColor} 
+            _hover={{ bg: buttonHoverBgColor }}
+          >
+            Consulte os agendamentos
+          </Button>
+        </Flex>
       </Flex>
     </Box>
   );
