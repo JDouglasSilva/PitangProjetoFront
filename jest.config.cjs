3cfbe299d5b579be9952edb03e589775f05d801a
Dag <jddouglassilva@gmail.com>
2024-07-15 15:22:46 -0300

    Instalação e configuração da biblioteca de testes

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Box, Button, Container, Flex, Heading, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
